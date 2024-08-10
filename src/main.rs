0545e0a0cdfd6d2f0edbb0af53c342671aa57e91
XavierLopez25 <thexlopez59@gmail.com>
2024-08-10 16:46:37 -0600

    feat: added maze rendering

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,31 @@ use framebuffer::Framebuffer;
 mod maze;
 use maze::load_maze;
 
+fn draw_cell(framebuffer: &mut Framebuffer, xo: usize, yo: usize, block_size: usize, cell: char) {
+    for x in xo..xo + block_size {
+        for y in yo..yo + block_size {
+            if (cell != ' ') {
+                framebuffer.point(x, y);
+            }
+        }
+    }
+}
+
 fn render2d(framebuffer: &mut Framebuffer) {
     let maze = load_maze("maze.txt");
+    let block_size = 100;
+
+    for row in 0..maze.len() {
+        for col in 0..maze[row].len() {
+            draw_cell(
+                framebuffer,
+                col * block_size,
+                row * block_size,
+                block_size,
+                maze[row][col],
+            );
+        }
+    }
 }
 
 fn main() {
@@ -40,6 +63,8 @@ fn main() {
         if window.is_key_down(minifb::Key::Escape) {
             break;
         }
+
+        framebuffer.clear();
 
         render2d(&mut framebuffer);
 
