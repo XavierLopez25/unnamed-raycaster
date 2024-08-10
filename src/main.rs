c2dd6914f54afbfdfd7cc9760b952fd2f65e2d12
XavierLopez25 <thexlopez59@gmail.com>
2024-08-10 17:15:15 -0600

    feat: added raycasting for the player and player rendering

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use minifb::{Key, Window, WindowOptions};
+use nalgebra_glm::Vec2;
 use std::f32::consts::PI;
 use std::time::Duration;
 
@@ -8,17 +9,35 @@ use framebuffer::Framebuffer;
 mod maze;
 use maze::load_maze;
 
+mod player;
+use player::Player;
+
+mod caster;
+use caster::{cast_ray, Intersect};
+
+fn cell_to_color(cell: char) -> u32 {
+    match cell {
+        '+' => 0xAA00AA,
+        '-' => 0x991199,
+        '|' => 0x881188,
+        'g' => 0xFF0000,
+        _ => 0x000000,
+    }
+}
+
 fn draw_cell(framebuffer: &mut Framebuffer, xo: usize, yo: usize, block_size: usize, cell: char) {
     for x in xo..xo + block_size {
         for y in yo..yo + block_size {
             if (cell != ' ') {
+                let color = cell_to_color(cell);
+                framebuffer.set_current_color(color);
                 framebuffer.point(x, y);
             }
         }
     }
 }
 
-fn render2d(framebuffer: &mut Framebuffer) {
+fn render2d(framebuffer: &mut Framebuffer, player: &Player) {
     let maze = load_maze("maze.txt");
     let block_size = 100;
 
@@ -33,6 +52,9 @@ fn render2d(framebuffer: &mut Framebuffer) {
             );
         }
     }
+    framebuffer.set_current_color(0x00FF00);
+    framebuffer.point(player.pos.x as usize, player.pos.y as usize);
+    cast_ray(framebuffer, &maze, player, player.angle, block_size, true);
 }
 
 fn main() {
@@ -58,6 +80,10 @@ fn main() {
     window.update();
 
     framebuffer.set_background_color(0x333355);
+    let player = Player {
+        pos: Vec2::new(150.0, 150.0),
+        angle: PI / 3.0,
+    };
 
     while window.is_open() {
         if window.is_key_down(minifb::Key::Escape) {
@@ -66,7 +92,7 @@ fn main() {
 
         framebuffer.clear();
 
-        render2d(&mut framebuffer);
+        render2d(&mut framebuffer, &player);
 
         window
             .update_with_buffer(&framebuffer.buffer, framebuffer.width, framebuffer.height)
